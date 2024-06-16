5ae27521bc7060c99c99620aed15dcb06b954e65
Gabriel Rocha <gabriel.drumond@gmail.com>
2024-06-16 14:38:47 +0000

    Inclusao Dockerfile

--- src/main/java/com/example/demo/SiscadApiApplication.java
+++ src/main/java/com/example/demo/SiscadApiApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-public class SiscadApiApplication {
+class DemoApplication
 
-	public static void main(String[] args) {
-		SpringApplication.run(SiscadApiApplication.class, args);
-	}
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
 
+@RestController
+class DemoController{
+    @GetMapping("/hello")
+    fun hello() : String {
+        return "Hello render"
+    }
 }
