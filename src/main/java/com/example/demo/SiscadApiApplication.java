ae369f86285a1c8d68ecc2ec5cd9b9b4bedccb1a
Gabriel Rocha <gabriel.drumond@gmail.com>
2024-06-17 01:28:36 +0000

    Correcoes

--- src/main/java/com/example/demo/SiscadApiApplication.java
+++ src/main/java/com/example/demo/SiscadApiApplication.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.runApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 class DemoApplication
